4b24c9f1b0a927d41e524b32cc4d65933b7bc915
Sachin <sachindilan@live.com>
2022-01-04 09:30:15 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -23,7 +23,6 @@ function getCarOptions(event) {
 		console.log(response);
 		console.log(response);
 		console.log(response);
-		console.log(response);
 		return sendResponse(200, JSON.stringify(response.options));
 	});
 }
