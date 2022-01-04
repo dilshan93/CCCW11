7ab109d44cf442fbc7c63850abab993e63069823
Sachin <sachindilan@live.com>
2022-01-04 19:05:52 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -21,6 +21,7 @@ function getCarOptions(event) {
 	const itemId = event.pathParameters.itemId;
 	return databaseManager.getItem(itemId).then(response => {
 		console.log(response);
+		console.log(response);
 		return sendResponse(200, JSON.stringify(response.options));
 	});
 }
